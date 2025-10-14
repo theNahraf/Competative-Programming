6e0f48a2e908ad404d6eb4595e6bf5bc3f1f8403
thenahraf <farhan.ug23@nsut.ac.in>
2025-10-14 21:22:31 +0530

    Segment Tree Class Implementatoin with private and public

--- CSES/Range Quaries/segmenttree.cpp
+++ CSES/Range Quaries/segmenttree.cpp
@@ -15,16 +15,10 @@ int mod =  1e9 + 7;
 
 
 class SegmentTree{
-public:
+private:
+      vector<ll> tree;
+      vector<ll> lazy;
     int n;
-    vector<ll> tree;
-
-    SegmentTree(vector<ll> arr){
-        int n = arr.size();
-        tree.resize(4*n);
-        build(0,0, n-1, arr);
-    }
-
     void build(int i, int l, int r, vector<ll> &arr){
         if(l==r){
             tree[i] =  arr[l];
@@ -36,7 +30,6 @@ public:
         build(2*i +2 , mid + 1, r, arr);
         tree[i] = tree[2*i +1] + tree[2*i + 2];
     }
-
 
     void updatequery(int idx, int value, int i, int l, int r){
         if(l==r){
@@ -50,15 +43,81 @@ public:
         tree[i] = tree[2*i + 1] + tree[2*i + 2];
     }
 
+      ll indvalue(int idx, int i, int l, int r){
+        if (lazy[i] != 0) {
+        tree[i] += (r - l + 1) * lazy[i];
+        if (l != r) {
+            lazy[2 * i + 1] += lazy[i];
+            lazy[2 * i + 2] += lazy[i];
+        }
+        lazy[i] = 0;
+         }
+
+        if(l==r){
+            return tree[i];
+        }
+        int mid = l  + (r-l)/2;
+        if(idx <= mid) return indvalue(idx, 2*i+1, l, mid);
+        return indvalue(idx, 2*i+2, mid + 1, r);
+    }
+
+    
+    void rangeupdate(int i, int l, int r, int st, int end, ll val){
+        if(lazy[i]!=0){
+            tree[i] += (r-l+1)*lazy[i];
+            if(l!=r){
+                lazy[2*i+1] += lazy[i];
+                lazy[2*i+2] += lazy[i];
+            }
+            lazy[i] = 0;
+        }
+        //out of the bound
+        if(r<st || l>end) return ;
+        
+        //occupied
+        if(l>=st && r<=end){
+            tree[i]+=(r-l+1)*val;
+            if(l!=r){
+                lazy[2*i+1] += val;
+                lazy[2*i+2] += val;
+            }
+            return ;
+        }
+
+        //overlapping
+        int mid = l + (r-l)/2;
+        
+        rangeupdate(2*i+1, l, mid, st, end, val);
+        rangeupdate(2*i+2, mid + 1, r, st, end, val);
+
+        tree[i] =  tree[2*i+1] + tree[2*i+2];
+    }
+
+
     ll rangesum(int i, int l, int r, int st, int end){
         if(l>end || r<st) return 0;
         if(l>=st && r<=end) return tree[i];
         int mid = (l+r)/2;
         return rangesum(2*i+1, l, mid, st, end) + rangesum(2*i+2, mid+1, r, st, end);
     }  
-    
 
-    
+public:
+    SegmentTree(vector<ll> &arr){
+        n =  arr.size();
+        tree.resize(4*n);
+        lazy.resize(4*n, 0);
+        build(0,0,n-1, arr);
+    }
+
+    ll getSum(int st, int end){
+        return rangesum(0, 0, n-1, st, end);
+    }
+
+    void setValue(int idx,  ll val){
+        updatequery(idx, val, 0, 0, n-1);
+    }
+
+
 };
 
 int main() {
@@ -73,7 +132,7 @@ int main() {
         cin>>q1>>q2;
         // rangesum calling
         
-        cout<<st.rangesum(0, 0, n-1, q1-1, q2-1)<<endl;
+        cout<<st.getSum(q1-1, q2-1)<<endl;
     }
     
     
